e888767bf8fc123f6bd43c1b3d8a2acfe7b487c6
Alex Morales Garcia <alex2521999@github.com>
2018-04-02 02:38:13 +0200

    Added Background Code, Lateral Spritesheet & Changed Background Spritesheet
    Added last part of the level, diagonal movement spritesheet and added sky to Background Spritesheet

--- SDL4_Handout/ModuleRender.cpp
+++ SDL4_Handout/ModuleRender.cpp
@@ -49,7 +49,7 @@ update_status ModuleRender::PreUpdate()
 
 update_status ModuleRender::Update()	
 {
-	int speed = 11;
+	int speed = 100;
 
 	if(App->input->keyboard[SDL_SCANCODE_UP] == 1)
 		camera.y += speed;
