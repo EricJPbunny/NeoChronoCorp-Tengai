91398873d76a42079765b4293df63fb772bbf3d2
AlexMG99 <alex2521999@github.com>
2018-06-01 16:34:58 +0200

    Added Module Select Screen & Spritesheet

---
+++ SDL9_Handout/ModuleSceneSelect.h
@@ -0,0 +1,36 @@
+#ifndef __MODULESCENESTART_H__
+#define __MODULESCENESTART_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "SDL_mixer/include/SDL_mixer.h"
+
+struct SDL_Texture;
+
+class ModuleSceneStart : public Module
+{
+public:
+	ModuleSceneStart();
+	~ModuleSceneStart();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* graphics = nullptr;
+	SDL_Texture* start_screen = nullptr;
+	SDL_Rect background, title, start;
+	SDL_Rect cloud, cloud1, cloud2, cloud3, cloud4, cloud5, cloud6, cloud7;
+	float posx = 0, posx1 = 0, posx2 = 0, posx3 = 0, posx4 = 0, posx5 = 0, posx6 = 0, posx7 = 0, posx8 = 0, posx9 = 0, posx10 = 0, posx11 = 0, posx12 = 0, posx13 = 0, posx14 = 0, posx15 = 0;
+	int alpha_start = 255;
+	int speed = 1;
+	bool fade = true;
+	Mix_Chunk* intro = nullptr;
+
+	Mix_Music* mus = nullptr;
+};
+
+#endif // __MODULESCENESTART_H__
