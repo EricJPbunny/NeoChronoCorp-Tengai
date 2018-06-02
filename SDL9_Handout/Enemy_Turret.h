0f121c19c0f6533056a17aad8cfd2c3f00c13bba
AlexMG99 <alex2521999@github.com>
2018-06-02 19:34:48 +0200

    Added Module Turret

---
+++ SDL9_Handout/Enemy_Turret.h
@@ -0,0 +1,27 @@
+#ifndef __ENEMY_TURRET_H__
+#define __ENEMY_TURRET_H__
+
+#include "Enemy.h"
+#include "Path.h"
+
+class Enemy_Turret :
+	public Enemy
+{
+public:
+	Enemy_Turret(int x, int y, int type);
+	~Enemy_Turret();
+
+	Collider* collider;
+	Path movement;
+
+private:
+
+	int type;
+	void Move();
+	void Draw(SDL_Texture* sprites);
+	SDL_Texture* boss_sprite;
+
+	Animation spawn, idle;
+};
+
+#endif
